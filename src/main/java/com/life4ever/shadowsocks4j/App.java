908e4b80b383846a312b04c11c270c09de3fe7f1
leo <leo.zhou.ke@gmail.com>
2021-04-15 21:34:36 +0800

    Initial commit

---
+++ src/main/java/com/life4ever/shadowsocks4j/App.java
@@ -0,0 +1,9 @@
+package com.life4ever.shadowsocks4j;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/life4ever/shadowsocks4j/AppTest.java
@@ -0,0 +1,14 @@
+package com.life4ever.shadowsocks4j;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
