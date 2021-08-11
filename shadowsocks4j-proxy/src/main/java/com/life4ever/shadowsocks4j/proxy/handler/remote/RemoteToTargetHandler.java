8fff1e291b467dcd6b2281a48584865529396357
leo <leo.zhou.ke@gmail.com>
2021-08-11 22:09:33 +0800

    2021.8.11 [proxy] 解决 remote-server CPU 占用率过高的问题

--- shadowsocks4j-proxy/src/main/java/com/life4ever/shadowsocks4j/proxy/handler/remote/RemoteToTargetHandler.java
+++ shadowsocks4j-proxy/src/main/java/com/life4ever/shadowsocks4j/proxy/handler/remote/RemoteToTargetHandler.java
@@ -1,5 +1,6 @@
 package com.life4ever.shadowsocks4j.proxy.handler.remote;
 
+import com.life4ever.shadowsocks4j.proxy.exception.Shadowsocks4jProxyException;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
@@ -15,7 +16,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class RemoteToTargetHandler extends ChannelInboundHandlerAdapter {
 
@@ -27,6 +32,10 @@ public class RemoteToTargetHandler extends ChannelInboundHandlerAdapter {
 
     private final AtomicReference<Channel> channelAtomicReference;
 
+    private final Lock lock = new ReentrantLock();
+
+    private final Condition condition = lock.newCondition();
+
     public RemoteToTargetHandler(NioEventLoopGroup workerGroup, InetSocketAddress targetServerInetSocketAddress, ChannelHandlerContext localChannelHandlerContext) {
         this.workerGroup = workerGroup;
         this.targetServerInetSocketAddress = targetServerInetSocketAddress;
@@ -37,13 +46,24 @@ public class RemoteToTargetHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
-        for (; ; ) {
-            Channel channel = channelAtomicReference.get();
-            if (channel != null) {
-                channel.writeAndFlush(byteBuf);
-                break;
+        lock.lock();
+        try {
+            Channel channel;
+            while ((channel = channelAtomicReference.get()) == null) {
+                if (!condition.await(5000L, TimeUnit.MILLISECONDS)) {
+                    throw new Shadowsocks4jProxyException("Failed to get channel within 5 seconds.");
+                }
             }
+            channel.writeAndFlush(byteBuf);
+        } finally {
+            lock.unlock();
         }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOG.error(cause.getMessage(), cause);
+        ctx.channel().close();
     }
 
     private void relayToTargetServer(ChannelHandlerContext localChannelHandlerContext) {
@@ -64,8 +84,14 @@ public class RemoteToTargetHandler extends ChannelInboundHandlerAdapter {
         bootstrap.connect(targetServerInetSocketAddress)
                 .addListener((ChannelFutureListener) future -> {
                     if (future.isSuccess()) {
-                        LOG.info("Succeed to connect to target server @ {}.", targetServerInetSocketAddress);
-                        channelAtomicReference.set(future.channel());
+                        lock.lock();
+                        try {
+                            LOG.info("Succeed to connect to target server @ {}.", targetServerInetSocketAddress);
+                            channelAtomicReference.set(future.channel());
+                            condition.signal();
+                        } finally {
+                            lock.unlock();
+                        }
                     } else {
                         LOG.error("Failed to connect to target server @ {}.", targetServerInetSocketAddress);
                     }
